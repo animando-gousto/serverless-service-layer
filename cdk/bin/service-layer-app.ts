a224a8084aeaf0e28cc7f2ca86b71b57bcc41ef8
Graham King <animandosolutions@gmail.com>
2021-04-12 11:11:01 +0100

    Tag resources

--- cdk/bin/service-layer-app.ts
+++ cdk/bin/service-layer-app.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
+import { Tags } from '@aws-cdk/core';
 import { ServiceLayerStack } from '../lib/service-layer-stack';
 
 const app = new cdk.App();
 
 const suffix = process.env.SUFFIX!
-new ServiceLayerStack(app, `ServiceLayer-${suffix}`, {
+const stack = new ServiceLayerStack(app, `ServiceLayer-${suffix}`, {
   suffix,
 });
+
+Tags.of(stack).add('env-suffix', suffix)
