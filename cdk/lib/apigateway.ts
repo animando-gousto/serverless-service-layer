f9b6065990e8740370dee53e9f558d58a026799b
Graham King <animandosolutions@gmail.com>
2021-04-15 16:30:02 +0100

    Try again with migration domain name

--- cdk/lib/apigateway.ts
+++ cdk/lib/apigateway.ts
@@ -34,7 +34,7 @@ export class ApiGateway extends cdk.Construct {
       handler: props.lambdas.apiLambda,
       proxy: false,
       domainName: {
-        domainName: `migration.${props.domainName}`,
+        domainName: `migration-${props.domainName}`,
         certificate,
       },
       restApiName: `ServiceLayer-${props.suffix}`,
